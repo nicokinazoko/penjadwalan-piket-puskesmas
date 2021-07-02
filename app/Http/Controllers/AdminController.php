00b16867a2f58e5788d5209c789c07eff18ad5d2
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-07-02 18:12:13 +0700

    Create AdminController.php

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //untuk lihat dashboard utama
+    public function viewDashboard()
+    {
+        return view('content.dashboard');
+    }
+
+    // Data Pegawai
+
+    //Untuk lihat data Pegawai
+    public function viewDataPegawai(){
+        return view('content.pegawai.pegawai-view-data');
+    }
+
+    //Untuk input data Pegawai
+    public function inputDataPegawai(){
+        return view('content.pegawai.pegawai-input-data');
+    }
+
+    //Untuk edit data Pegawai
+    public function editDataPegawai(){
+        return view('content.pegawai.pegawai-edit-data');
+    }
+
+
+    //Data Piket
+
+    //untuk melihat data piket
+    public function viewDataPiket(){
+        return view('content.piket.piket-view-data');
+    }
+
+    //untuk input data piket
+    public function inputDataPiket(){
+        return view('content.piket.piket-input-data');
+    }
+
+    //untuk edit data piket
+    public function editDataPiket(){
+        return view('content.piket.edit-data');
+    }
+
+}
