16e0ee3a49c3eb279f23fbcf8ef0ae508d907a31
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-07-06 18:39:14 +0700

    Update footer-scripts.blade.php

--- resources/views/layouts/footer/footer-scripts.blade.php
+++ resources/views/layouts/footer/footer-scripts.blade.php
@@ -1,21 +1,21 @@
 <!-- jQuery -->
 <script src="{{ asset('vendor/plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{{asset('vendor/plugins/jquery-ui/jquery-ui.min.js')}}}"></script>
+{{-- <script src="{{ asset('vendor/plugins/jquery-ui/jquery-ui.min.js') }}"></script> --}}
 
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
+{{-- <script>
     $.widget.bridge('uibutton', $.ui.button)
-</script>
+</script> --}}
 <!-- Bootstrap 4 -->
 <script src="{{ asset('vendor/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- ChartJS -->
 <script src="{{ asset('vendor/plugins/chart.js/Chart.min.js') }}"></script>
-<!-- Sparkline -->
-<script src="{{ asset('vendor/plugins/sparklines/sparkline.js') }}"></script>
-<!-- JQVMap -->
+{{-- <!-- Sparkline -->
+<script src="{{ asset('vendor/plugins/sparklines/sparkline.js') }}"></script> --}}
+{{-- <!-- JQVMap -->
 <script src="{{ asset('vendor/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
-<script src="{{ asset('vendor/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+<script src="{{ asset('vendor/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script> --}}
 <!-- jQuery Knob Chart -->
 <script src="{{ asset('vendor/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 <!-- daterangepicker -->
@@ -33,3 +33,24 @@
 <script src="{{ asset('vendor/adminlte/dist/js/demo.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{ asset('vendor/adminlte/dist/js/pages/dashboard.js') }}"></script>
+
+<!--Form -->
+<!-- Select2 -->
+<script src="{{ asset('vendor/plugins/select2/js/select2.full.min.js') }}"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="{{ asset('vendor/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js') }}"></script>
+<!-- InputMask -->
+<script src="{{ asset('vendor/plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('vendor/plugins/inputmask/jquery.inputmask.min.js') }}"></script>
+<!-- date-range-picker -->
+<script src="{{ asset('vendor/plugins/daterangepicker/daterangepicker.js') }}"></script>
+<!-- bootstrap color picker -->
+<script src="{{ asset('vendor/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js') }}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{ asset('vendor/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+<!-- Bootstrap Switch -->
+<script src="{{ asset('vendor/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}"></script>
+<!-- BS-Stepper -->
+<script src="{{ asset('vendor/plugins/bs-stepper/js/bs-stepper.min.js') }}"></script>
+<!-- dropzonejs -->
+<script src="{{ asset('vendor/plugins/dropzone/min/dropzone.min.js') }}"></script>
