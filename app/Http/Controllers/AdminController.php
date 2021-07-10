84566e610d39f05f1def6dd795132f18fc81182f
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-07-10 19:03:20 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,36 +15,52 @@ class AdminController extends Controller
     // Data Pegawai
 
     //Untuk lihat data Pegawai
-    public function viewDataPegawai(){
+    public function viewDataPegawai()
+    {
         return view('content.pegawai.pegawai-view-data');
     }
 
     //Untuk input data Pegawai
-    public function inputDataPegawai(){
+    public function inputDataPegawai()
+    {
         return view('content.pegawai.pegawai-input-data');
     }
 
-    //Untuk edit data Pegawai
-    public function editDataPegawai(){
+    //Untuk view edit data Pegawai
+    public function editDataPegawai()
+    {
         return view('content.pegawai.pegawai-edit-data');
+    }
+
+    // untuk edit data pegawai base id
+    public function editDataPegawaiById()
+    {
+        return view('content.pegawai.pegawai-edit-data-id');
     }
 
 
     //Data Piket
 
     //untuk melihat data piket
-    public function viewDataPiket(){
+    public function viewDataPiket()
+    {
         return view('content.piket.piket-view-data');
     }
 
     //untuk input data piket
-    public function inputDataPiket(){
+    public function inputDataPiket()
+    {
         return view('content.piket.piket-input-data');
     }
 
-    //untuk edit data piket
-    public function editDataPiket(){
+    //untuk view edit data piket
+    public function editDataPiket()
+    {
         return view('content.piket.piket-edit-data');
     }
 
+    public function editDataPiketById()
+    {
+        return view('content.piket.piket-edit-data-id');
+    }
 }
