852ec47ed1d9ffd7aca9c5e366549cdfdc47d79e
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-07-19 19:39:24 +0700

    Update piket-input-data.blade.php

--- resources/views/content/piket/piket-input-data.blade.php
+++ resources/views/content/piket/piket-input-data.blade.php
@@ -32,33 +32,18 @@
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
-                    <form>
+                    <form method="GET" action="#">
+                        @csrf
                         <div class="card-body">
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                <label for="inputKodePiket">Kode Piket</label>
+                                <input type="text" class="form-control" id="inputKodePiket" placeholder="Kode Piket"
+                                    name="inputKodePiket">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1"
-                                    placeholder="Password">
-                            </div>
-
-                            <div class="form-group">
-                                <label>Minimal</label>
-                                <select class="form-control select2bs4" style="width: 100%;">
-                                    <option selected="selected">Alabama</option>
-                                    <option>Alaska</option>
-                                    <option>California</option>
-                                    <option>Delaware</option>
-                                    <option>Tennessee</option>
-                                    <option>Texas</option>
-                                    <option>Washington</option>
-                                </select>
-                            </div>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                <label for="inputNamaPiket">Nama Piket</label>
+                                <input type="text" class="form-control" id="inputNamaPiket" placeholder="Nama Piket"
+                                    name="inputNamaPiket">
                             </div>
                         </div>
                         <!-- /.card-body -->
