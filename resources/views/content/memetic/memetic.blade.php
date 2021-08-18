8fc58939ea7283e5b667c4849ddf1b18d078672a
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-18 19:24:01 +0700

    Update memetic.blade.php

--- resources/views/content/memetic/memetic.blade.php
+++ resources/views/content/memetic/memetic.blade.php
@@ -2,74 +2,65 @@
 
 @section('title', 'SPP | Proses Algoritma Memetika')
 @section('content')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Advanced Form</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
-                            {{-- <li class="breadcrumb-item"><a href="{{ route('piket-view-data') }}">Data Piket</a></li> --}}
-                            <li class="breadcrumb-item active">Algoritma Memetika</li>
-                        </ol>
-                    </div>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Input Data Algoritma Memetika</h1>
                 </div>
-            </div><!-- /.container-fluid -->
-        </section>
-
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                {{-- form --}}
-                <div class="card card-default">
-                    <div class="card-header">
-                        <h3 class="card-title">Quick Example</h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <!-- form start -->
-                    <form>
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1"
-                                    placeholder="Password">
-                            </div>
-
-                            <div class="form-group">
-                                <label>Minimal</label>
-                                <select class="form-control select2bs4" style="width: 100%;">
-                                    <option selected="selected">Alabama</option>
-                                    <option>Alaska</option>
-                                    <option>California</option>
-                                    <option>Delaware</option>
-                                    <option>Tennessee</option>
-                                    <option>Texas</option>
-                                    <option>Washington</option>
-                                </select>
-                            </div>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                        </div>
-                        <!-- /.card-body -->
-
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+                        {{-- <li class="breadcrumb-item"><a href="{{ route('piket-view-data') }}">Data Piket</a></li> --}}
+                        <li class="breadcrumb-item active">Algoritma Memetika</li>
+                    </ol>
                 </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-                <h5 class="mt-4 mb-2">Data Pegawai yang Sudah Ada</h5>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            {{-- form --}}
+            <div class="card card-default">
+                <div class="card-header">
+                    <h3 class="card-title">Data Algoritma Memetika</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form method="POST" action="{{ route('proses-memetika') }}">
+                    @csrf
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="inputJumlahPopulasi">Jumlah Populasi</label>
+                            <input type="text" class="form-control" id="inputJumlahPopulasi"
+                                placeholder="Jumlah Populasi" name="inputJumlahPopulasi">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputMutationRate">Mutation Rate</label>
+                            <input type="text" class="form-control" id="inputMutationRate" placeholder="Mutation Rate"
+                            name="inputMutationRate">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputBulanPiket">Bulan Piket</label>
+                            <input type="month" class="form-control" id="inputBulanPiket" placeholder="Bulan Piket"
+                            name="inputBulanPiket">
+                        </div>
+
+                    </div>
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+
+            {{-- <h5 class="mt-4 mb-2">Data Pegawai yang Sudah Ada</h5>
                 <div class="row">
                     <div class="col-12">
 
@@ -117,19 +108,19 @@
                         <!-- /.card -->
                     </div>
                     <!-- /.col -->
-                </div>
+                </div> --}}
 
-            </div>
-            <!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
 
 @endsection
 
 
 @section('addons-scripts')
-    @include('layouts.footer.form-scripts')
-    @include('layouts.footer.table-data-scripts')
+@include('layouts.footer.form-scripts')
+@include('layouts.footer.table-data-scripts')
 @endsection
