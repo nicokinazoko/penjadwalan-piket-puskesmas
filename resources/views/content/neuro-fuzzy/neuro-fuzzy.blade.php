31bd01d66e21581587d0ea6ccea413b697b3bf5d
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-08-24 19:41:12 +0700

    Update neuro-fuzzy.blade.php

--- resources/views/content/neuro-fuzzy/neuro-fuzzy.blade.php
+++ resources/views/content/neuro-fuzzy/neuro-fuzzy.blade.php
@@ -28,38 +28,30 @@
                 {{-- form --}}
                 <div class="card card-default">
                     <div class="card-header">
-                        <h3 class="card-title">Quick Example</h3>
+                        <h3 class="card-title">Data Algoritma Neuro Fuzzy</h3>
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
-                    <form>
+
+                    <form method="POST" action="{{ route('proses-neuro-fuzzy') }}">
+                        @csrf
                         <div class="card-body">
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                <label for="inputJumlahPopulasi">Jumlah Populasi</label>
+                                <input type="text" class="form-control" id="inputJumlahPopulasi"
+                                    placeholder="Jumlah Populasi" name="inputJumlahPopulasi">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1"
-                                    placeholder="Password">
+                                <label for="inputMutationRate">Mutation Rate</label>
+                                <input type="text" class="form-control" id="inputMutationRate" placeholder="Mutation Rate"
+                                    name="inputMutationRate">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputBulanPiket">Bulan Piket</label>
+                                <input type="month" class="form-control" id="inputBulanPiket" placeholder="Bulan Piket"
+                                    name="inputBulanPiket">
                             </div>
 
-                            <div class="form-group">
-                                <label>Minimal</label>
-                                <select class="form-control select2bs4" style="width: 100%;">
-                                    <option selected="selected">Alabama</option>
-                                    <option>Alaska</option>
-                                    <option>California</option>
-                                    <option>Delaware</option>
-                                    <option>Tennessee</option>
-                                    <option>Texas</option>
-                                    <option>Washington</option>
-                                </select>
-                            </div>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
                         </div>
                         <!-- /.card-body -->
 
@@ -67,57 +59,9 @@
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
                     </form>
+
                 </div>
 
-                <h5 class="mt-4 mb-2">Data Pegawai yang Sudah Ada</h5>
-                <div class="row">
-                    <div class="col-12">
-
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">DataTable with default features</h3>
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body">
-                                <table id="example1" class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Nama Pegawai</th>
-                                            <th>Jenis Kelamin</th>
-                                            <th>Jabatan</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($pegawai as $dataPegawai)
-                                            <tr>
-                                                <td>{{ $dataPegawai->nama_pegawai }}</td>
-                                                <td>{{ $dataPegawai->nama_jenis_kelamin }}</td>
-                                                <td>{{ $dataPegawai->nama_jabatan }}</td>
-                                                <td>
-                                                    <a href="#"><i class="fas fa-edit"></i></a>
-                                                    <a href="#"><i class="fas fa-trash-alt"></i></a>
-                                                </td>
-                                            </tr>
-
-                                        @endforeach
-                                    </tbody>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Nama Pegawai</th>
-                                            <th>Jenis Kelamin</th>
-                                            <th>Jabatan</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </tfoot>
-                                </table>
-                            </div>
-                            <!-- /.card-body -->
-                        </div>
-                        <!-- /.card -->
-                    </div>
-                    <!-- /.col -->
-                </div>
 
             </div>
             <!-- /.container-fluid -->
