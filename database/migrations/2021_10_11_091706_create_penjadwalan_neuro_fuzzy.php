33ff8d8882485da3b6241107c0af8063126f8bcf
nicokinazoko <nicodunois017157@gmail.com>
2021-10-11 17:41:43 +0700

    Create 2021_10_11_091706_create_penjadwalan_neuro_fuzzy.php

---
+++ database/migrations/2021_10_11_091706_create_penjadwalan_neuro_fuzzy.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenjadwalanNeuroFuzzy extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjadwalan_neuro_fuzzy', function (Blueprint $table) {
+            $table->id('id_penjadwalan_neuro_fuzzy');
+            $table->unsignedBigInteger('id_pegawai');
+            $table->unsignedBigInteger('id_piket');
+            $table->date('tanggal_penjadwalan');
+            // $table->dateTime('tanggal_pembuatan_jadwal');
+
+            $table->foreign('id_pegawai')->references('id_pegawai')->on('pegawais');
+            $table->foreign('id_piket')->references('id_piket')->on('pikets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjadwalan_neuro_fuzzy');
+    }
+}
