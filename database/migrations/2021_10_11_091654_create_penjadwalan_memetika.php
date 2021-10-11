1b460d88d1ddb6d45b993a1fa97dec8a248f4357
nicokinazoko <nicodunois017157@gmail.com>
2021-10-11 17:41:41 +0700

    Create 2021_10_11_091654_create_penjadwalan_memetika.php

---
+++ database/migrations/2021_10_11_091654_create_penjadwalan_memetika.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenjadwalanMemetika extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjadwalan_memetika', function (Blueprint $table) {
+            $table->id('id_penjadwalan_memetika');
+            $table->unsignedBigInteger('id_pegawai');
+            $table->unsignedBigInteger('id_piket');
+            $table->date('tanggal_penjadwalan');
+            // $table->dateTime('tanggal_pembuatan_jadwal');
+
+            $table->foreign('id_pegawai')->references('id_pegawai')->on('pegawais');
+            $table->foreign('id_piket')->references('id_piket')->on('pikets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjadwalan_memetika');
+    }
+}
