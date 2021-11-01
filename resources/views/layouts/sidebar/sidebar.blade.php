eb97878f05e3bc8dadd5ec1a77fce73548e06176
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-11-01 16:57:35 +0700

    Update sidebar.blade.php

--- resources/views/layouts/sidebar/sidebar.blade.php
+++ resources/views/layouts/sidebar/sidebar.blade.php
@@ -139,7 +139,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="{{route('view-data-algoritma-neuro-fuzzy')}}" class="nav-link">
                             <i class="far fa-circle nav-icon"></i>
                             <p>Algoritma Neuro Fuzzy</p>
                         </a>
