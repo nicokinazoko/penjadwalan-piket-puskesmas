3a0654ac124ae1d96124202a1250cc0b9b4c609a
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-11-01 15:14:26 +0700

    Create view-data-neuro-fuzzy.blade.php

---
+++ resources/views/content/neuro-fuzzy/view-data-neuro-fuzzy.blade.php
@@ -0,0 +1,118 @@
+@extends('master.master')
+
+@section('title', 'SPP | Lihat Data Penjadwalan Algoritma Neuro Fuzzy')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Hasil Algoritma Neuro Fuzzy</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+                            {{-- <li class="breadcrumb-item"><a href="{{ route('piket-view-data') }}">Data Piket</a></li> --}}
+                            <li class="breadcrumb-item active"><a href="{{ route('view-memetika') }}">Algoritma
+                                    Neuro Fuzzy</a>
+                            </li>
+                            <li class="breadcrumb-item active">View Data Algoritma Neuro Fuzzy</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                {{-- form --}}
+                <div class="card">
+                    <div class="card-header d-flex p-0">
+                        <h3 class="card-title p-3">Hasil Proses Algoritma Neuro Fuzzy</h3>
+                        <ul class="nav nav-pills ml-auto p-2">
+                            <li class="nav-item"><a class="nav-link active" href="#tab_1" data-toggle="tab">Hasil</a>
+                            </li>
+                            {{-- <li class="nav-item"><a class="nav-link" href="#tab_2" data-toggle="tab">Populasi
+                                    Awal</a>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="#tab_3" data-toggle="tab">Populasi
+                                    Akhir</a>
+                            <li class="nav-item"><a class="nav-link" href="#tab_4" data-toggle="tab">Fitness
+                                    Populasi Awal</a>
+                            <li class="nav-item"><a class="nav-link" href="#tab_5" data-toggle="tab">Fitness
+                                    Populasi Akhir</a> --}}
+                        </ul>
+                    </div><!-- /.card-header -->
+                    <div class="card-body">
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab_1">
+
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center">Tanggal Jadwal</th>
+                                            <th class="text-center">Tanggal Pembuatan </th>
+                                            <th class="text-center">Aksi </th>
+                                        </tr>
+
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($dataPembuatanJadwal as $dataTanggalPembuatanJadwal)
+                                            <tr>
+
+                                                {{-- $waktuPembuatanBaru = date("Y-m-d H:i:s", strtotime($waktuPembuatan)); --}}
+                                                <td>{{ date('F Y', strtotime($dataTanggalPembuatanJadwal->tanggal)) }}
+                                                </td>
+                                                <td><a
+                                                        href="{{ route('view-data-penjadwalan-algoritma-neuro-fuzzy', ['tanggal_pembuatan' => $dataTanggalPembuatanJadwal->tanggal_pembuatan_jadwal]) }}">
+                                                        {{ date('d F Y H:i:s', strtotime($dataTanggalPembuatanJadwal->tanggal_pembuatan_jadwal)) }}
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    <center>
+                                                        <a
+                                                            href="{{ route('delete-data-penjadwalan-neuro-fuzzy', ['tanggal_pembuatan' => $dataTanggalPembuatanJadwal->tanggal_pembuatan_jadwal]) }}"><i
+                                                                class="fas fa-trash-alt"></i></a>
+                                                    </center><a </td>
+
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th class="text-center">Tanggal Jadwal</th>
+                                            <th class="text-center">Tanggal Pembuatan </th>
+                                            <th class="text-center">Aksi </th>
+                                        </tr>
+
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <!-- /.tab-pane -->
+                        </div>
+                        <!-- /.tab-content -->
+                    </div><!-- /.card-body -->
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+@endsection
+
+
+@section('addons-scripts')
+    {{-- @include('layouts.footer.form-scripts') --}}
+    @include('layouts.footer.table-data-scripts')
+    <script>
+        /* Initialization of datatables */
+        $(document).ready(function() {
+            $('table.display').DataTable();
+        });
+    </script>
+@endsection
