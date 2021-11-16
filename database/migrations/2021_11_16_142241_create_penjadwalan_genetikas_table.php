485cadc3940dab617840072b4d1bfd2279bf57a0
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-11-16 22:15:59 +0700

    Create 2021_11_16_142241_create_penjadwalan_genetikas_table.php

---
+++ database/migrations/2021_11_16_142241_create_penjadwalan_genetikas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenjadwalanGenetikasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjadwalan_genetikas', function (Blueprint $table) {
+            $table->id('id_penjadwalan_genetikas');
+            $table->unsignedBigInteger('id_pegawai');
+            $table->unsignedBigInteger('id_piket');
+            $table->string('kode_piket');
+            $table->date('tanggal_penjadwalan');
+            $table->dateTime('tanggal_pembuatan_jadwal');
+            $table->foreign('id_pegawai')->references('id_pegawai')->on('pegawais');
+            $table->foreign('id_piket')->references('id_piket')->on('pikets');
+            $table->string('nilai_fitness');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjadwalan_genetikas');
+    }
+}
